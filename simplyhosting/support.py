709ea6516ab5348a786061fc3db6de1d0a963fea
Bernardo Silva <bsilva@names.co.uk>
2017-05-12 00:35:58 +0100

    #10 - begin implementing support resource API

--- simplyhosting/support.py
+++ simplyhosting/support.py
@@ -1,3 +1,50 @@
 class Support(object):
     def __init__(self, apiClient):
         self.apiClient = apiClient
+
+    def get_active_tickets(self, **kwargs):
+        request = Request('post', '/support/getActiveTickets')
+        client_id = kwargs.get('client_id', '')
+        from_date = kwargs.get('from_date', '')
+        to_date = kwargs.get('to_date', '')
+        server_id = kwargs.get('server_id', '')
+        service_id = kwargs.get('service_id', '')
+        status = kwargs.get('status', '')
+
+        if client_id:
+            request.data['clientId'] = client_id
+        if from_date:
+            request.data['fromDate'] = from_date
+        if to_date:
+            request.data['toDate'] = to_date
+        if server_id:
+            request.data['serverId'] = server_id
+        if service_id:
+            request.data['serviceId'] = service_id
+
+        self.apiClient.request = request
+        return self.apiClient
+
+    def create_ticket(self, subject, text, **kwargs):
+        request = Request('post', '/support/createTicket')
+        request.data = {'subject': subject, 'text': text}
+
+        server_id = kwargs.get('server_id', '')
+        service_id = kwargs.get('service_id', '')
+        cc = kwargs.get('cc', '')
+        priority = kwargs.get('priority', '')
+        ticket_type = kwargs.get('type', '')
+
+        if service_id:
+            request.data['serviceId'] = service_id
+        if server_id:
+            request.data['serverId'] = server_id
+        if cc:
+            request.data['cc'] = cc
+        if priority:
+            request.data['priority'] = priority
+        if ticket_type:
+            request.data['type'] = ticket_type
+
+        self.apiClient.request = request
+        return self.apiClient
