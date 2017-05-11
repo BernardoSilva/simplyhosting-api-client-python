9c9efb751e94d7e32aab01ac8f8f1de7db5b0c66
Bernardo Silva <bsilva@names.co.uk>
2017-05-11 16:10:29 +0100

    Add tests to ensure new client logic is working

--- test/test_client.py
+++ test/test_client.py
@@ -3,17 +3,25 @@ import unittest
 
 class Test_client(unittest.TestCase):
     def test_client_api_key_attribute_is_required(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(ValueError):
             client = Client()
 
-    def test_client_api_key_can_be_set(self):
+    def test_client_api_keys_can_be_set(self):
         my_api_key = 'test-key'
-        client = Client(my_api_key)
+        my_api_secret = 'my-secret'
+        client = Client(api_key=my_api_key, api_secret=my_api_secret)
         self.assertEqual(my_api_key, client.api_key)
+        self.assertEqual(my_api_secret, client.api_secret)
+
+    def test_client_api_user_credentials_can_be_set(self):
+        my_username = 'bsilva@names.co.uk'
+        my_password = 'my-super-password'
+        client = Client(username=my_username, password=my_password)
+        self.assertEqual(my_username, client.username)
+        self.assertEqual(my_password, client.password)
 
     def test_client_user_method_return_user_resource(self):
-        my_api_key = 'test-key'
-        client = Client(my_api_key)
+        client = Client(api_key='a', api_secret='b')
         user_object = client.user()
         self.assertEqual('User', user_object.__class__.__name__)
 
