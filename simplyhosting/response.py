eb71741a86e09cc29757afa8a837adb0c7ac408e
Bernardo Silva <bsilva@names.co.uk>
2017-05-12 16:17:44 +0100

    remove print from Response object wrapper

--- simplyhosting/response.py
+++ simplyhosting/response.py
@@ -3,7 +3,6 @@ class Response(object):
         response handling
     """
     def __init__(self, requests_response):
-        print('got requests_response object:', requests_response)
         self.content = requests_response.json()
         self.status_code = requests_response.status_code
         self.original_response = requests_response
