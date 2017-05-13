b8e3956d79c69af6b26a0426ae61950f9979bc63
Bernardo Silva <benny.stuff@gmail.com>
2017-05-13 18:41:04 +0100

    refactor support api to accept dictionaries to reduce complexity on testing and implementation
    when passing a dictionary it is already very expressive what data we are passing to the function
    so the effort of using kwargs and having to validate each one does not worth the effort.

--- simplyhosting/support.py
+++ simplyhosting/support.py
@@ -5,53 +5,16 @@ class Support(object):
     def __init__(self, apiClient):
         self.apiClient = apiClient
 
-    def get_active_tickets(self, **kwargs):
+    def get_active_tickets(self, optional_data={}):
         request = Request('post', '/support/getActiveTickets')
-        client_id = kwargs.get('client_id', '')
-        from_date = kwargs.get('from_date', '')
-        to_date = kwargs.get('to_date', '')
-        server_id = kwargs.get('server_id', '')
-        service_id = kwargs.get('service_id', '')
-        status = kwargs.get('status', '')
-
-        if client_id:
-            request.data['clientId'] = client_id
-        if from_date:
-            request.data['fromDate'] = from_date
-        if to_date:
-            request.data['toDate'] = to_date
-        if server_id:
-            request.data['serverId'] = server_id
-        if service_id:
-            request.data['serviceId'] = service_id
-        if status is not None:
-            request.data['status'] = status
-
+        request.data = optional_data
         self.apiClient.request = request
         return self.apiClient
 
-    def get_tickets(self, limit, **kwargs):
+    def get_tickets(self, limit, optional_data={}):
         request = Request('post', '/support/getTickets')
-        client_id = kwargs.get('client_id', '')
-        from_date = kwargs.get('from_date', '')
-        to_date = kwargs.get('to_date', '')
-        server_id = kwargs.get('server_id', '')
-        service_id = kwargs.get('service_id', '')
-        status = kwargs.get('status', '')
         request.data = {'limit': limit}
-
-        if client_id:
-            request.data['clientId'] = client_id
-        if from_date:
-            request.data['fromDate'] = from_date
-        if to_date:
-            request.data['toDate'] = to_date
-        if server_id:
-            request.data['serverId'] = server_id
-        if service_id:
-            request.data['serviceId'] = service_id
-        if status is not None:
-            request.data['status'] = status
+        request.data.update(optional_data)
         self.apiClient.request = request
         return self.apiClient
 
@@ -61,8 +24,11 @@ class Support(object):
         self.apiClient.request = request
         return self.apiClient
 
-    def get_ticket(self):
-        pass
+    def get_ticket(self, ticket_id):
+        request = Request('post', '/support/getTicket')
+        request.data = {'ticketId': ticket_id}
+        self.apiClient.request = request
+        return self.apiClient
 
     def reply_ticket(self):
         pass
@@ -73,26 +39,10 @@ class Support(object):
     def close_ticket(self):
         pass
 
-    def create_ticket(self, subject, text, **kwargs):
+    def create_ticket(self, subject, text, optional_data={}):
         request = Request('post', '/support/createTicket')
         request.data = {'subject': subject, 'text': text}
-
-        server_id = kwargs.get('server_id', '')
-        service_id = kwargs.get('service_id', '')
-        cc = kwargs.get('cc', '')
-        priority = kwargs.get('priority', '')
-        ticket_type = kwargs.get('type', '')
-
-        if service_id:
-            request.data['serviceId'] = service_id
-        if server_id:
-            request.data['serverId'] = server_id
-        if cc:
-            request.data['cc'] = cc
-        if priority:
-            request.data['priority'] = priority
-        if ticket_type:
-            request.data['type'] = ticket_type
+        request.data.update(optional_data)
 
         self.apiClient.request = request
         return self.apiClient
--- test/test_support.py
+++ test/test_support.py
@@ -5,62 +5,25 @@ import unittest
 class Test_support(unittest.TestCase):
     def setUp(self):
         self.client = Client(api_key='a', api_secret='b')
+        self.optional_data={'optionalParam': 'value'}
 
     def test_get_active_tickets_set_all_fields_correctly(self):
-        self.client.support().get_active_tickets(
-            client_id=1,
-            from_date='2017-05-13',
-            to_date='2017-05-15',
-            server_id=2,
-            service_id=3,
-            status=0
-        )
+        self.client.support().get_active_tickets(self.optional_data)
 
         request = self.client.request
-        self.assertEqual(1, request.data['clientId'])
-        self.assertEqual('2017-05-13', request.data['fromDate'])
-        self.assertEqual('2017-05-15', request.data['toDate'])
-        self.assertEqual(2, request.data['serverId'])
-        self.assertEqual(3, request.data['serviceId'])
-        self.assertEqual(0, request.data['status'])
+        self.assertEqual('value', request.data['optionalParam'])
 
     def test_get_tickets_set_all_fields_correctly(self):
-        self.client.support().get_tickets(
-            20,
-            client_id=1,
-            from_date='2017-05-13',
-            to_date='2017-05-15',
-            server_id=2,
-            service_id=3,
-            status=0
-        )
+        self.client.support().get_tickets(20, self.optional_data)
 
         request = self.client.request
         self.assertEqual(20, request.data['limit'])
-        self.assertEqual(1, request.data['clientId'])
-        self.assertEqual('2017-05-13', request.data['fromDate'])
-        self.assertEqual('2017-05-15', request.data['toDate'])
-        self.assertEqual(2, request.data['serverId'])
-        self.assertEqual(3, request.data['serviceId'])
-        self.assertEqual(0, request.data['status'])
+        self.assertEqual('value', request.data['optionalParam'])
 
     def test_search_set_all_fields_correctly(self):
-        self.client.support().search({
-            'fromDate': '2017-05-13',
-            'toDate': '2017-05-15',
-            'serverId': 2,
-            'clientId': 1,
-            'serverLabel': 'dev',
-            'text': 'example'
-        })
-
+        self.client.support().search(self.optional_data)
         request = self.client.request
-        self.assertEqual('2017-05-13', request.data['fromDate'])
-        self.assertEqual('2017-05-15', request.data['toDate'])
-        self.assertEqual(2, request.data['serverId'])
-        self.assertEqual(1, request.data['clientId'])
-        self.assertEqual('dev', request.data['serverLabel'])
-        self.assertEqual('example', request.data['text'])
+        self.assertEqual('value', request.data['optionalParam'])
 
     def test_support_create_ticket_set_all_fields_correctly(self):
         ticket_subject = 'Server is down'
@@ -68,18 +31,15 @@ class Test_support(unittest.TestCase):
         self.client.support().create_ticket(
             ticket_subject,
             ticket_body,
-            server_id=123,
-            service_id=1234,
-            cc='group@email.com',
-            priority=2,
-            type=4
+            self.optional_data
         )
 
         request = self.client.request
         self.assertEqual(ticket_subject, request.data['subject'])
         self.assertEqual(ticket_body, request.data['text'])
-        self.assertEqual(123, request.data['serverId'])
-        self.assertEqual(1234, request.data['serviceId'])
-        self.assertEqual('group@email.com', request.data['cc'])
-        self.assertEqual(2, request.data['priority'])
-        self.assertEqual(4, request.data['type'])
+        self.assertEqual('value', request.data['optionalParam'])
+
+    def test_get_ticket_set_all_fields_correctly(self):
+        self.client.support().get_ticket(69)
+        request = self.client.request
+        self.assertEqual(69, request.data['ticketId'])
