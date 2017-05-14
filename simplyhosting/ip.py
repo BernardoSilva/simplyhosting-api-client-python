bddae058258743b5e9a409b1f093328b2d981cee
Bernardo Silva <benny.stuff@gmail.com>
2017-05-14 18:18:04 +0100

    implement /ip/nullRoute

--- simplyhosting/ip.py
+++ simplyhosting/ip.py
@@ -17,3 +17,10 @@ class IP(object):
         request.data = {'ip': ip}
         self.apiClient.request = request
         return self.apiClient
+
+    def null_route(self, ip, optional_data={}):
+        request = Request('post', '/ip/nullRoute')
+        request.data = {'ip': ip}
+        request.data.update(optional_data)
+        self.apiClient.request = request
+        return self.apiClient
--- test/test_ip.py
+++ test/test_ip.py
@@ -16,3 +16,9 @@ class Test_ip(unittest.TestCase):
         self.client.ip().get_ptr('192.168.0.1')
         request = self.client.request
         self.assertEqual('192.168.0.1', request.data['ip'])
+
+    def test_null_route_set_data_successfully(self):
+        self.client.ip().null_route('192.168.0.1', self.optional_data)
+        request = self.client.request
+        self.assertEqual('192.168.0.1', request.data['ip'])
+        self.assertEqual('value', request.data['optionalParam'])
