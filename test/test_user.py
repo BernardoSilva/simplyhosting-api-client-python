cf7e1111da7081b6257a10ffafbd55e437f97d56
Bernardo Silva <benny.stuff@gmail.com>
2017-05-14 17:28:03 +0100

    add test for /user/auth

---
+++ test/test_user.py
@@ -0,0 +1,14 @@
+from simplyhosting.client import Client
+import unittest
+
+class Test_user(unittest.TestCase):
+    def setUp(self):
+        self.client = Client(api_key='a', api_secret='b')
+        self.optional_data={'optionalParam': 'value'}
+
+    def test_auth_set_data_successfully(self):
+        self.client.user().auth('benny', 'my-secret-password')
+
+        request = self.client.request
+        self.assertEqual('benny', request.data['login'])
+        self.assertEqual('my-secret-password', request.data['password'])
