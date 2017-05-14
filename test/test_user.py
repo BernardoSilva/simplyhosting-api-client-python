6392085347a83aa84eb9a780c95b442cead43e56
Bernardo Silva <benny.stuff@gmail.com>
2017-05-14 17:44:30 +0100

    add simple test to ensure /user/ping is correct

--- test/test_user.py
+++ test/test_user.py
@@ -8,7 +8,11 @@ class Test_user(unittest.TestCase):
 
     def test_auth_set_data_successfully(self):
         self.client.user().auth('benny', 'my-secret-password')
-
         request = self.client.request
         self.assertEqual('benny', request.data['login'])
         self.assertEqual('my-secret-password', request.data['password'])
+
+    def test_ping_path_is_correct(self):
+        self.client.user().ping()
+        request = self.client.request
+        self.assertEqual('/user/ping', request.path)
